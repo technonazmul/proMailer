d68775cb3f1d71aecda4527de04cf76c5b33ab4d
Md Nazmul Khalid <41265922+technonazmul@users.noreply.github.com>
2024-07-15 22:52:32 +0600

    migration update

--- database/migrations/2024_07_14_194123_create_mail_templates_table.php
+++ database/migrations/2024_07_14_194123_create_mail_templates_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('mail_templates', function (Blueprint $table) {
             $table->id();
+            $table->string("name")->nullable();
+            $table->text("template")->nullable();
             $table->timestamps();
         });
     }
