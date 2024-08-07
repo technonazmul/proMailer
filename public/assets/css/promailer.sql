2d959d6d2f2ff38e763e3f46b184741f1328fc05
Md Nazmul Khalid <41265922+technonazmul@users.noreply.github.com>
2024-08-07 21:05:58 +0600

    testing

--- public/assets/css/promailer.sql
+++ public/assets/css/promailer.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jul 31, 2024 at 02:14 PM
+-- Generation Time: Aug 07, 2024 at 03:05 PM
 -- Server version: 8.2.0
 -- PHP Version: 8.2.13
 
